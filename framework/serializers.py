97c721bd279a9ffd575befa3087a581fe7a1e049
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2022-06-03 22:22:11 -0400

    makeshift resourcethroughtopic serializer

--- framework/serializers.py
+++ framework/serializers.py
@@ -3,22 +3,26 @@ from rest_framework import serializers
 from django.db.models import fields
 from .models import *
 
-class ThroughSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ResourceThroughTopic
-        fields = ['order', 'topic', 'resources']
-        depth = 0
-
 class ResourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resource
-        fields = ['id', 'link']
+        fields = ['link']
 
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
-        fields = ['id', 'topicName', 'relatedResources']
-        depth = 2
+        fields = ['topicName']
+
+class ThroughSerializer(serializers.ModelSerializer):
+
+    # topic = serializers.SerializerMethodField()
+    resources = ResourceSerializer()
+    topic = TopicSerializer()
+
+    class Meta:
+        model = ResourceThroughTopic
+        fields = ['order', 'topic', 'resources']
+        depth = 0
 
 # returning the ordered list for resources according to topic
 
