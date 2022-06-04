13d156eb6e6bf1013490d892242686fb802a60c5
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2022-06-03 22:56:32 -0400

    Perfected Topic Serializer with resource ID

--- framework/serializers.py
+++ framework/serializers.py
@@ -4,20 +4,36 @@ from django.db.models import fields
 from .models import *
 
 class ResourceSerializer(serializers.ModelSerializer):
+
+    # order = serializers.SerializerMethodField()
+
+    order = serializers.SerializerMethodField('is_named_bar')
+
+    def is_named_bar(self, Resource):
+        through = ResourceThroughTopic.objects.get(resources=Resource.id)
+        order = through.order
+        return order
+
     class Meta:
         model = Resource
-        fields = ['link']
+        fields = ['order', 'link']
+    
+    # def get_order(self, obj):
+    #     Order = ResourceThroughTopic.objects.get(resource=obj.id)
+    #     Order = Order.order
+    #     return Order
 
 class TopicSerializer(serializers.ModelSerializer):
+    relatedResources = ResourceSerializer(many=True)
     class Meta:
         model = Topic
-        fields = ['topicName']
+        fields = ['topicName', 'relatedResources']
 
 class ThroughSerializer(serializers.ModelSerializer):
 
     # topic = serializers.SerializerMethodField()
-    resources = ResourceSerializer()
-    topic = TopicSerializer()
+    # resources = ResourceSerializer()
+    # topic = TopicSerializer()
 
     class Meta:
         model = ResourceThroughTopic
